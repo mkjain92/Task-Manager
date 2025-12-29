dc99ddc309ec0e95cfe879665063c9d02f1ce7b0
manish kumar jain <manishkumarjain71@gmail.com>
2025-12-29 21:42:50 +0530

    middleware to handle possible errors

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,20 +1,23 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs"
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const {name, email, password, profileImageUrl, adminJoinCode} = req.body
 
     if(!name || !email || !password || name === "" || email === "" || password === "") {
-        return res.status(400).json({ message: "All fileds are required"});
+        //return res.status(400).json({ message: "All fileds are required"});
+        return next(errorHandler(400, "All fileds are required"));
     }
 
     //check if user already exists
     const isAlreadyExist = await User.findOne({ email });
 
     if(isAlreadyExist) {
-        return res
-          .status(400)
-          .json({ success: false, message: "User already exists"})
+        return next(errorHandler(400, "User already exists"))
+        // return res
+        //   .status(400)
+        //   .json({ success: false, message: "User already exists"})
     }
 
     // check user role
@@ -38,7 +41,8 @@ export const signup = async (req, res) => {
         await newUser.save()
         res.json("Signup successful")
     } catch (error) {
-        res.status(500).json({message: error.message()})
+        next(error.message);
+        //res.status(500).json({message: error.message()})
     }
 
 }
--- backend/index.js
+++ backend/index.js
@@ -35,3 +35,16 @@ app.use("/api/auth", authRoutes)
 app.listen(3000, ()=> {
     console.log("Serve is running on port 3000!");
 })
+
+// Error Heandlar Middleware
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500
+
+    const message = err.message || "Internal Server Error"
+
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
