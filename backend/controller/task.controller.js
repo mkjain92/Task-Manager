e16fdffb6c473158143638fb1c5fb2457eb74669
manish kumar jain <manishkumarjain71@gmail.com>
2026-01-13 22:00:23 +0530

    get dashboard data (admin) api route

--- backend/controller/task.controller.js
+++ backend/controller/task.controller.js
@@ -2,105 +2,105 @@ import Task from "../models/task.model.js"
 import { errorHandler } from "../utils/error.js"
 
 export const createTask = async (req, res, next) => {
-    try {
-        const {
-            title,
-            description,
-            priority,
-            dueDate,
-            assignedTo,
-            attachments,
-            todoChecklist,
-        } = req.body
+  try {
+    const {
+      title,
+      description,
+      priority,
+      dueDate,
+      assignedTo,
+      attachments,
+      todoChecklist,
+    } = req.body
 
-        if(!Array.isArray(assignedTo)) {
-            return next(errorHandler(400, "assignedTo must be an array of user IDs"))
-        }
-
-        const task = await Task.create({
-            title,
-            description,
-            priority,
-            dueDate,
-            assignedTo,
-            attachments,
-            todoChecklist,
-            createdBy: req.user.id,
-        })
-        res.status(201).json({ message: "Task created successfully", task})
-    } catch (error) {
-        next(error)
+    if (!Array.isArray(assignedTo)) {
+      return next(errorHandler(400, "assignedTo must be an array of user IDs"))
     }
+
+    const task = await Task.create({
+      title,
+      description,
+      priority,
+      dueDate,
+      assignedTo,
+      attachments,
+      todoChecklist,
+      createdBy: req.user.id,
+    })
+    res.status(201).json({ message: "Task created successfully", task })
+  } catch (error) {
+    next(error)
+  }
 }
 
 export const getTasks = async (req, res, next) => {
-    try {
-      const { status } = req.query
-      let filter = {}
-      if(status) {
-        filter.status = status
-      }
-      let tasks
-      if(req.user.role === "admin") {
-        tasks = await Task.find(filter).populate(
-            "assignedTo",
-            "name email profileImageUrl"
-        )
-      } else {
-        tasks = await Task.find({
-            ...filter,
-            assignedTo: req.user.id,
-        }).populate("assignedTo", "name email profileImageUrl")
-      }
-
-      tasks = await Promise.all(
-        tasks.map(async (task) => {
-            const completedCount = task.todoChecklist.filter(
-                (item) => item.completed
-            ).length
-            return {...task._doc, completedCount: completedCount}
-        })
-      )
-
-      // status summary count
-
-      const allTasks = await Task.countDocuments(
-        req.user.role === "admin" ? {} : { assignedTo: req.user.id }
-      )
-
-      const pendingTasks = await Task.countDocuments({
-        ...filter,
-        status: "Pending",
-        // if logged in user is not admin then add assignedTo filter
-        // if logged in user is an admin then nothing to do, just count
-        ...(req.user.role !== "admin" && { assignedTo: req.user.id }),
-      })
-
-      const inProgressTasks = await Task.countDocuments({
-        ...filter,
-        status: "In Progress",
-        ...(req.user.role !== "admin" && { assignedTo: req.user.id }),
-      })
-
-      const completedTasks = await Task.countDocuments({
-        ...filter,
-        status: "Completed",
-        ...(req.user.role !== "admin" && { assignedTo: req.user.id }),
-      })
-
-      res.status(200).json({
-        tasks,
-        statusSummary: {
-          all: allTasks, 
-          pendingTasks,
-          inProgressTasks,
-          completedTasks
-        },
-      })
-
-    } catch (error) {
-      next(error)
+  try {
+    const { status } = req.query
+    let filter = {}
+    if (status) {
+      filter.status = status
     }
+    let tasks
+    if (req.user.role === "admin") {
+      tasks = await Task.find(filter).populate(
+        "assignedTo",
+        "name email profileImageUrl"
+      )
+    } else {
+      tasks = await Task.find({
+        ...filter,
+        assignedTo: req.user.id,
+      }).populate("assignedTo", "name email profileImageUrl")
+    }
+
+    tasks = await Promise.all(
+      tasks.map(async (task) => {
+        const completedCount = task.todoChecklist.filter(
+          (item) => item.completed
+        ).length
+        return { ...task._doc, completedCount: completedCount }
+      })
+    )
+
+    // status summary count
+
+    const allTasks = await Task.countDocuments(
+      req.user.role === "admin" ? {} : { assignedTo: req.user.id }
+    )
+
+    const pendingTasks = await Task.countDocuments({
+      ...filter,
+      status: "Pending",
+      // if logged in user is not admin then add assignedTo filter
+      // if logged in user is an admin then nothing to do, just count
+      ...(req.user.role !== "admin" && { assignedTo: req.user.id }),
+    })
+
+    const inProgressTasks = await Task.countDocuments({
+      ...filter,
+      status: "In Progress",
+      ...(req.user.role !== "admin" && { assignedTo: req.user.id }),
+    })
+
+    const completedTasks = await Task.countDocuments({
+      ...filter,
+      status: "Completed",
+      ...(req.user.role !== "admin" && { assignedTo: req.user.id }),
+    })
+
+    res.status(200).json({
+      tasks,
+      statusSummary: {
+        all: allTasks,
+        pendingTasks,
+        inProgressTasks,
+        completedTasks
+      },
+    })
+
+  } catch (error) {
+    next(error)
+  }
 }
 
 export const getTaskById = async (req, res, next) => {
@@ -123,7 +123,7 @@ export const updateTask = async (req, res, next) => {
   try {
     const task = await Task.findById(req.params.id)
 
-    if(!task) {
+    if (!task) {
       return next(errorHandler(404, "Task not found!"))
     }
 
@@ -135,7 +135,7 @@ export const updateTask = async (req, res, next) => {
     task.attachments = req.body.attachments || task.attachments
 
     if (req.body.assignedTo) {
-      if(!Array.isArray(req.body.assignedTo)) {
+      if (!Array.isArray(req.body.assignedTo)) {
         return next(
           errorHandler(400, "assignedTo must be an array of user IDs")
         )
@@ -143,13 +143,13 @@ export const updateTask = async (req, res, next) => {
 
       task.assignedTo = req.body.assignedTo
     }
-    
+
     const updateTask = await task.save()
 
     return res
       .status(200)
-      .json({ updateTask, message: "Task updated successfully!"})
-  } catch(error) {
+      .json({ updateTask, message: "Task updated successfully!" })
+  } catch (error) {
     next(error);
   }
 }
@@ -164,8 +164,8 @@ export const deleteTask = async (req, res, next) => {
 
     await task.deleteOne()
 
-    res.status(200).json({message: "Task deleted successfully!"})
-  } catch(error) {
+    res.status(200).json({ message: "Task deleted successfully!" })
+  } catch (error) {
     next(error)
   }
 }
@@ -174,7 +174,7 @@ export const updateTaskStatus = async (req, res, next) => {
   try {
     const task = await Task.findById(req.params.id)
 
-    if(!task) {
+    if (!task) {
       return next(errorHandler(404, "Task not found!"));
     }
 
@@ -188,65 +188,133 @@ export const updateTaskStatus = async (req, res, next) => {
 
     task.status = req.body.status || task.status
 
-    if(task.status === "Completed") {
+    if (task.status === "Completed") {
       task.todoChecklist.forEach((item) => (item.completed = true))
     }
 
     await task.save()
 
-    res.status(200).json({ message: "Task status updated", task})
+    res.status(200).json({ message: "Task status updated", task })
 
-  } catch(error) {
+  } catch (error) {
     next(error)
   }
 }
 
 export const updateTaskChecklist = async (req, res, next) => {
   try {
-      const {todoChecklist} = req.body
+    const { todoChecklist } = req.body
 
-      const task = await Task.findById(req.params.id)
+    const task = await Task.findById(req.params.id)
 
-      if(!task) {
-        return next(errorHandler(404, "Task not found!"))
-      }
+    if (!task) {
+      return next(errorHandler(404, "Task not found!"))
+    }
 
-      if(!task.assignedTo.includes(req.user.id) && req.user.role !== "admin") {
-        return next(errorHandler(403, "Not authorized to update checklist"))
-      }
+    if (!task.assignedTo.includes(req.user.id) && req.user.role !== "admin") {
+      return next(errorHandler(403, "Not authorized to update checklist"))
+    }
 
-      task.todoChecklist = todoChecklist;
+    task.todoChecklist = todoChecklist;
 
-      const completedCount = task.todoChecklist.filter(
-        (item) => item.completed
-      ).length
+    const completedCount = task.todoChecklist.filter(
+      (item) => item.completed
+    ).length
 
-      const totalItems = task.todoChecklist.length;
+    const totalItems = task.todoChecklist.length;
 
-      task.progress = totalItems > 0 ? Math.round((completedCount / totalItems) * 100) : 0;
+    task.progress = totalItems > 0 ? Math.round((completedCount / totalItems) * 100) : 0;
 
-      // 4 task, 1 complete completedCount / totalItems =  (1/4) * 100 = 25
+    // 4 task, 1 complete completedCount / totalItems =  (1/4) * 100 = 25
 
-      if(task.progress === 100) {
-        task.status = "Completed"
-      }
-      else if (task.progress > 0) {
-        task.status = "In Progress"
-      }
-      else {
-        task.status = "Pending"
-      }
+    if (task.progress === 100) {
+      task.status = "Completed"
+    }
+    else if (task.progress > 0) {
+      task.status = "In Progress"
+    }
+    else {
+      task.status = "Pending"
+    }
 
-      await task.save();
+    await task.save();
 
-      const updatedTask = await Task.findById(req.params.id).populate(
-        "assignedTo",
-        "name email profileImageUrl"
-      )
+    const updatedTask = await Task.findById(req.params.id).populate(
+      "assignedTo",
+      "name email profileImageUrl"
+    )
 
-      res
+    res
       .status(200)
-      .json({ message: "Task checklist updated", task: updatedTask})
+      .json({ message: "Task checklist updated", task: updatedTask })
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const getDashboardData = async (req, res, next) => {
+  try {
+    // Fetch statistic
+    const totalTasks = await Task.countDocuments()
+    const pendingTasks = await Task.countDocuments({ status: "Pending" })
+    const completedTasks = await Task.countDocuments({ status: "Completed" })
+    const overdueTasks = await Task.countDocuments({
+      status: { $ne: "Completed" },
+      dueDate: { $lt: new Date() },
+    })
+    const taskStatuses = ["Pending", "In Progress", "Completed"]
+
+    const taskDistributionRaw = await Task.aggregate([
+      {
+        $group: {
+          _id: "$status",
+          count: { $sum: 1},
+        }
+      }
+    ])
+
+    const taskDistribution = taskStatuses.reduce((acc, status) => {
+      const formattedkey = status.replace(/\s+/g, "") //remove spaces for response keys
+      acc[formattedkey] = taskDistributionRaw.find((item) => item._id === status)?.count || 0
+      return acc
+    }, {})
+
+    taskDistribution["All"] = totalTasks
+    const taskPriorities = ["Low", "Medium", "High"]
+    const taskPriorityLevelRaw = await Task.aggregate([
+      {
+        $group: {
+          _id: "$priority",
+          count: { $sum:1 }, 
+        },
+      },
+    ])
+    const taskPriorityLevel = taskPriorities.reduce((acc, priority) => {
+      acc[priority] = taskPriorityLevelRaw.find((item) => item._id === priority)?.count || 0
+      return acc
+    },{})
+
+    // Fetch recent 10 tasks
+    const recentTasks = await Task.find()
+    .sort({ createdAt: -1 })
+    .limit(10)
+    .select("title status priority dueDate createdAt")
+
+    res.status(200).json({
+      statistics: {
+        totalTasks,
+        pendingTasks,
+        completedTasks,
+        overdueTasks,
+      },
+      charts: {
+        taskDistribution,
+        taskPriorityLevel
+      },
+
+      recentTasks,
+    })
 
   } catch (error) {
     next(error)
